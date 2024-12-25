6ac5906b72e424c69abe2ac02a1c135cd79c65dc
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-12-25 19:57:24 +0530

    add sign-out functionality and implement sidebar and topbar components

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -15,8 +15,8 @@ export const useSignInAccount = () => {
     });
   };
   
-//   export const useSignOutAccount = () => {
-//     return useMutation({
-//       mutationFn: signOutAccount,
-//     });
-//   };
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
+  });
+};
